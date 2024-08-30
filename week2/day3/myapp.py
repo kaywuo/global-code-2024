472d13848bc1f85354280b0a9a79c7af68293c2b
kaywuo <britwumansah@gmail.com>
2024-08-30 11:14:23 +0000

    text

--- week2/day3/myapp.py
+++ week2/day3/myapp.py
@@ -5,9 +5,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'ansah britwum kwabena\n am 13 years old\n i love Ghana'
+    return 'ansah britwum kwabena'
+
+
+@app.route('/whereami')
+def whereami():
+    return 'Ghana!'
+
+from flask import Flask, render_template
+@app.route('/g')
+def indexg():
+    return render_template('index.html')
+
+
+from flask import Flask, render_template
+
+@app.route('/foo/<name>')
+def foo(name):
+    return render_template('index.html', to=name)
+
+@app.route("/myname/<name>")
+def cool(name):
+    print(name)
+    return render_template("myname.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
 
+    app.run(debug=True, host='0.0.0.0')
+ 
 
